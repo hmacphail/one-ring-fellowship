d179bae1cd274711d730afdd79c0cf5ea5d5a3c1
Heather MacPhail <hmacphail@gmail.com>
2018-08-16 23:36:09 -0700

    ts linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FellowshipUndertakingComponent } from "./fellowship-undertaking/fellowship-undertaking.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FellowshipUndertakingComponent,
+    ],
+    imports: [
+        BrowserModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/fellowship-undertaking/fellowship-undertaking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-fellowship-undertaking",
+    templateUrl: "./fellowship-undertaking.component.html",
+    styleUrls: ["./fellowship-undertaking.component.scss"],
+})
+export class FellowshipUndertakingComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
